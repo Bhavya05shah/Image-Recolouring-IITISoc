cbb99af56cc9181b92e5dff8ad28c5caffe98c42
Bhavya Shah <142381024+Bhavya05shah@users.noreply.github.com>
2024-09-01 19:20:27 +0530

    Update LAB Color Space.py

--- LAB Color Space.py
+++ LAB Color Space.py
@@ -4,16 +4,21 @@ import numpy as np
 #Models from the github repo of richzhang
 protoxt_path = 'Models/colorization_deploy_v2.prototxt'#specifies the neural network
 model_path = 'Models/colorization_release_v2.caffemodel'#contains the trained weights and parameters
-kernel_path = 'models/pts_in_hull.npy'#helps in storing predefined colors and also mapping RGB to LAB
+kernel_path = 'models/pts_in_hull.npy'#helps in storing predefined colors and also mapping RGB to LAB , contains color clusters
 img_path = 'Photos/bw.jpg'
 
-# Loaded the pre-trained network 
+#Loaded the pre-trained network 
 network = cv.dnn.readNetFromCaffe(protoxt_path, model_path)
 points = np.load(kernel_path)
 
-# Process the points(?)
+#Process the points(?)
+#this 313 means that there are 313 color clusters and each color cluster is treated as a single point(1,1)
 points = points.transpose().reshape(2, 313, 1, 1)
-network.getLayer(network.getLayerId("class8_ab")).blobs = [points.astype(np.float32)]
+#class8_ab is the layer from the network getting extracted and points/clusters are being assigned to the blobs(which stores the weights)
+network.getLayer(network.getLayerId("class8_ab")).blobs = [points.astype(np.float32)]#the float32 is the required format for this network
+#here another network layer operating at the end which is responsible for refining the predictions of the ab channels
+#a numoy array where there is a single output for each of the 313 color clusters and a bias of 2.606 (which is an experimentally determined value)
+#gets added to the each of 313 raw scores from this layer and then gets converted to probabilities
 network.getLayer(network.getLayerId('conv8_313_rh')).blobs = [np.full([1, 313], 2.606, np.float32)]
 
 # Load and process the black and white image
@@ -22,9 +27,9 @@ normalized = bw_img.astype("float32") / 255.0 #converting the values from 0 to 2
 lab = cv.cvtColor(normalized, cv.COLOR_BGR2LAB)#converting the image to LAB color space
 
 # Resize the L channel and subtract 50
-resized = cv.resize(lab, (224, 224))
+resized = cv.resize(lab, (224, 224))#here the model is designed so to have an input of 224x224 pixel image
 L = cv.split(resized)[0]#splitting the resized image into l,a,b and storing the l image 
-L -= 50# for reducing the brightness
+L -= 50# for reducing the brightness and centered around 0
 
 # Set the input for the network and forward pass(?)
 network.setInput(cv.dnn.blobFromImage(L))
